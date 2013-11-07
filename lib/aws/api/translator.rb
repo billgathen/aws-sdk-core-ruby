e479b9173be15b4923d9359c9b0e3ff0cfd0c3d3
Trevor Rowe <trevorrowe@gmail.com>
2013-11-07 15:51:38 -0800

    Added the response paging plugin to each service.

--- lib/aws/api/translator.rb
+++ lib/aws/api/translator.rb
@@ -12,6 +12,7 @@ module Aws
         Aws::Plugins::RetryErrors
         Aws::Plugins::GlobalConfiguration
         Aws::Plugins::RegionalEndpoint
+        Aws::Plugins::ResponsePaging
         Aws::Plugins::Credentials
       )
 
